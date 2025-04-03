f531ee627b3885eb7f412de96b760bbee2439d5d
Prathmesh Ramesh Gangarde <116508620+pratham643@users.noreply.github.com>
2025-04-03 17:34:18 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,9 +7,9 @@ import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your
 // Splash Screen
 
 const splashScreen = {
-  enabled: false, // set false to disable splash screen
+  enabled: true, // set false to disable splash screen
   animation: splashAnimation,
-  duration: 9000 // Set animation duration as per your animation
+  duration: 2000 // Set animation duration as per your animation
 };
 
 // Summary And Greeting Section
